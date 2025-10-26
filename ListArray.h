df325ec6e458ba70692c820434762468569a70b8
AntoniRagaMocholí <antoniwintermono@gmail.com>
2025-10-26 13:17:40 +0100

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,17 +1,19 @@
 #ifndef LISTARRAY_H
 #define LISTARRAY_H
 
-#include "List.h"
+#include <ostream>
 #include <stdexcept>
+#include "List.h"
 
 template <typename T>
 class ListArray : public List<T> {
 private:
-    T* data;
-    int capacity;
-    int count;
+    T* arr;
+    int max;
+    int n;
+    static const int MINSIZE = 2;
 
-    void resize(int new_capacity);
+    void resize(int new_size);
 
 public:
     ListArray();
@@ -25,48 +27,61 @@ public:
     int search(T e) const override;
     bool empty() const override;
     int size() const override;
+
+    T operator[](int pos);
+    friend std::ostream& operator<<(std::ostream& out, const ListArray<T>& list) {
+        out << "[";
+        for (int i = 0; i < list.n; ++i) {
+            out << list.arr[i];
+            if (i < list.n - 1) out << ", ";
+        }
+        out << "]";
+        return out;
+    }
 };
+
+// Implementación
 
 template <typename T>
 ListArray<T>::ListArray() {
-    capacity = 10;
-    count = 0;
-    data = new T[capacity];
+    arr = new T[MINSIZE];
+    max = MINSIZE;
+    n = 0;
 }
 
 template <typename T>
 ListArray<T>::~ListArray() {
-    delete[] data;
+    delete[] arr;
 }
 
 template <typename T>
-void ListArray<T>::resize(int new_capacity) {
-    T* new_data = new T[new_capacity];
-    for (int i = 0; i < count; ++i)
-        new_data[i] = data[i];
-    delete[] data;
-    data = new_data;
-    capacity = new_capacity;
+void ListArray<T>::resize(int new_size) {
+    T* nuevo = new T[new_size];
+    for (int i = 0; i < n; ++i)
+        nuevo[i] = arr[i];
+    delete[] arr;
+    arr = nuevo;
+    max = new_size;
 }
 
 template <typename T>
 void ListArray<T>::insert(int pos, T e) {
-    if (pos < 0 || pos > count)
+    if (pos < 0 || pos > n)
         throw std::out_of_range("Posición inválida");
 
-    if (count == capacity)
-        resize(capacity * 2);
+    if (n == max)
+        resize(max * 2);
 
-    for (int i = count; i > pos; --i)
-        data[i] = data[i - 1];
+    for (int i = n; i > pos; --i)
+        arr[i] = arr[i - 1];
 
-    data[pos] = e;
-    ++count;
+    arr[pos] = e;
+    ++n;
 }
 
 template <typename T>
 void ListArray<T>::append(T e) {
-    insert(count, e);
+    insert(n, e);
 }
 
 template <typename T>
@@ -76,39 +91,51 @@ void ListArray<T>::prepend(T e) {
 
 template <typename T>
 T ListArray<T>::remove(int pos) {
-    if (pos < 0 || pos >= count)
+    if (pos < 0 || pos >= n)
         throw std::out_of_range("Posición inválida");
 
-    T removed = data[pos];
-    for (int i = pos; i < count - 1; ++i)
-        data[i] = data[i + 1];
-    --count;
-    return removed;
+    T eliminado = arr[pos];
+    for (int i = pos; i < n - 1; ++i)
+        arr[i] = arr[i + 1];
+    --n;
+
+    if (n < max / 4 && max / 2 >= MINSIZE)
+        resize(max / 2);
+
+    return eliminado;
 }
 
 template <typename T>
 T ListArray<T>::get(int pos) const {
-    if (pos < 0 || pos >= count)
+    if (pos < 0 || pos >= n)
         throw std::out_of_range("Posición inválida");
-    return data[pos];
+    return arr[pos];
 }
 
 template <typename T>
 int ListArray<T>::search(T e) const {
-    for (int i = 0; i < count; ++i)
-        if (data[i] == e)
+    for (int i = 0; i < n; ++i)
+        if (arr[i] == e)
             return i;
     return -1;
 }
 
 template <typename T>
 bool ListArray<T>::empty() const {
-    return count == 0;
+    return n == 0;
 }
 
 template <typename T>
 int ListArray<T>::size() const {
-    return count;
+    return n;
+}
+
+template <typename T>
+T ListArray<T>::operator[](int pos) {
+    if (pos < 0 || pos >= n)
+        throw std::out_of_range("Posición inválida");
+    return arr[pos];
 }
 
 #endif
+
